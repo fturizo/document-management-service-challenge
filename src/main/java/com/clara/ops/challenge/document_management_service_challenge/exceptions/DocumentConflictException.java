0e891ade83813b1efe9786504f03ee4a5cd7d363
Fabio Turizo <fabio.turizo@gmail.com>
2026-03-24 14:28:34 -0500

    refactor: Updated DocumentConflictException class
    Improved consistency with other custom exception classes

--- src/main/java/com/clara/ops/challenge/document_management_service_challenge/exceptions/DocumentConflictException.java
+++ src/main/java/com/clara/ops/challenge/document_management_service_challenge/exceptions/DocumentConflictException.java
@@ -2,12 +2,18 @@ package com.clara.ops.challenge.document_management_service_challenge.exceptions
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Exception thrown when attempting to create or upload a document that already exists in the system.
+ * <br>
+ * This runtime exception is typically used in scenarios where a document with the specified name
+ * conflicts with an existing document. It provides a message detailing the name of the conflicting document.
+ * <br>
+ * Extends {@link RuntimeException} to represent an unchecked exception.
+ */
 @RequiredArgsConstructor
 public class DocumentConflictException extends RuntimeException {
 
-  private final String documentName;
-
-  public String getMessage() {
-    return String.format("Document with name %s already exists", documentName);
-  }
+    public DocumentConflictException(String documentName){
+        super("Document with name %s already exists".formatted(documentName));
+    }
 }
